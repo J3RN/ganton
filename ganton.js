3bd6b803f7381d4e6cc43642efb1b86ff29a329c
afuhrtrumpet@gmail.com <afuhrtrumpet@gmail.com>
2015-01-29 16:40:00 -0800

    Initial function working

--- ganton.js
+++ ganton.js
@@ -1,5 +1,5 @@
 var config = {
-	channels: ["#osuosc"],
+	channels: ["#cwdg"],
 	server: "irc.freenode.net",
 	botName: "ganton"
 };
@@ -15,26 +15,30 @@ var bot = new irc.Client(config.server, config.botName, {
 });
 
 bot.addListener("message", function(from, to, text, message) {
-	if (text.substring(0, 3) == ".gh") {
-		var repo = text.substring(4);
-		console.log(repo);
+	if (text.indexOf("<find-dot-number") == 0) {
+		var dotnumber= text.substring("<find-dot-number".length + 1);
+		console.log(dotnumber);
 		var url = base_url; 
-		url = url.concat(encodeURIComponent(repo));
+		url = url.concat("name_n=").concat(encodeURIComponent(dotnumber));
 		console.log(url);
 		request({
 			url: url,
-			json: true,
 			headers: {
 				'User-Agent': 'Ganton by J3RN'
 			}
 		}, function(error, response, body) {
 			if (!error && response.statusCode == 200) {
-				var json = body;
-				if (json.items.length > 0) {
-					var message = json.items[0].html_url;
+				var json_response = JSON.parse(body);
+				console.log(JSON.stringify(json_response));
+				if (json_response.length > 0) {
+					var person= json_response[0];
+					message = person.display_name + " has the email " + person.username;
+					for (var i in person.majors) {
+						message += ", majors in " + person.majors[i].major;
+					}
 					bot.say(to, message);
 				} else {
-					bot.say(to, "No repos found.");
+					bot.say(to, "No data found");
 				}
 			}
 			else if (error) {
