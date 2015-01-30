a8f2e5ebd6d33a0cc229cde0005b46aab67922f8
Jonathan Arnett <jonarnett90@gmail.com>
2015-01-29 20:00:38 -0500

    It's better now! I promise!

--- ganton.js
+++ ganton.js
@@ -1,7 +1,7 @@
 var config = {
 	channels: ["#cwdg"],
 	server: "irc.freenode.net",
-	botName: "ganton"
+	botName: "ganton-test"
 };
 
 var irc = require("irc");
@@ -14,7 +14,7 @@ var bot = new irc.Client(config.server, config.botName, {
 	channels: config.channels
 });
 
-function replyPerson(param, value) {
+function getPerson(param, value) {
     var url = base_url;
     url = url.concat(param + "=").concat(encodeURIComponent(value));
 
@@ -31,34 +31,47 @@ function replyPerson(param, value) {
             var json_response = JSON.parse(body);
             console.log(JSON.stringify(json_response));
             if (json_response.length > 0) {
-                var person= json_response[0];
-                message = person.display_name + " has the email " + person.username;
-                for (var i in person.majors) {
-                    message += ", majors in " + person.majors[i].major;
-                }
-                bot.say(to, message);
-            } else {
-                bot.say(to, "No data found");
+                var person = json_response[0];
+                return person;
             }
-        }
-        else if (error) {
+        } else if (error) {
             console.log(error);
         } else {
             console.log(response.statusCode);
         }
+
+        return undefined;
     });
+}
+
+function sendReply(bot, to, person) {
+    if (person) {
+        var message = person.display_name + " has the email " + person.username;
+        for (var i in person.majors) {
+            message += ", majors in " + person.majors[i].major;
+        }
+        bot.say(to, message);
+    } else {
+        bot.say(to, "No data found");
+    }
 }
 
 bot.addListener("message", function(from, to, text, message) {
 	if (text.indexOf("<find-dot-number") == 0) {
 		var dotnumber= text.substring("<find-dot-number".length + 1);
 		console.log(dotnumber);
-        replyPerson("name_n", dotnumber);
-    } else (text.substring(0, 11) == "<find-fname") {
+
+        var person = getPerson("name_n", dotnumber);
+        sendReply(bot, to, person);
+    } else if (text.substring(0, 11) == "<find-fname") {
         var fname = text.match(/\<find-fname (.*)/)[1];
-        replyPerson("firstname", fname);
-    } else (text.substring(0, 11) == "<find-lname") {
+
+        var person = getPerson("firstname", fname);
+        sendReply(bot, to, person);
+    } else if (text.substring(0, 11) == "<find-lname") {
         var lname = text.match(/\<find-lname (.*)/)[1];
-        replyPerson("lastname", lname);
+
+        var person = getPerson("lastname", lname);
+        sendReply(bot, to, person);
     }
 });
