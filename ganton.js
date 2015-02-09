778cd6841ff802ad3a4a61e785af04cbb722969e
Alex Fuhr <aftrumpet@opensource.osu.edu>
2015-02-09 09:23:57 -0500

    Added support for private messaging

--- ganton.js
+++ ganton.js
@@ -65,7 +65,7 @@ function sendReply(bot, to, person) {
     }
 }
 
-bot.addListener("message", function(from, to, text, message) {
+function matchText(text, target) {
     params = {};
 
     if (text.match(/\<dot-number (\w+\.\d+)/)) {
@@ -81,6 +81,14 @@ bot.addListener("message", function(from, to, text, message) {
     }
 
     if (Object.keys(params).length) {
-        stalkPerson(params, bot, to);
+        stalkPerson(params, bot, target);
     }
+};
+
+bot.addListener("pm", function(from, text, message) {
+	matchText(text, from);
+});
+
+bot.addListener("message#", function(from, to, text, message) {
+	matchText(text, to);
 });
